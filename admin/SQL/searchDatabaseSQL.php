57590a90e9291085c6d4a3f2ddca7b9e4f6274af
Nick Liffen <UP653591@myport.ac.uk>
2014-04-02 22:46:27 +0100

    Admin - Stock Levels nearly complete

---
+++ admin/SQL/searchDatabaseSQL.php
@@ -0,0 +1,30 @@
+<?php
+
+include("../../database/connect_database.php");
+  //Brings in the string that they have searhed for.
+   $searchString = $_GET['str'];
+    //Runs a query to find that product based on the string. SEARCHES UNDER NAME AND DESCRIPTION
+    $query = "SELECT id, product_name, product_quantity, product_description, product_category, product_price FROM products WHERE product_quantity < '$searchString'";
+    $result = $database->query($query) OR die("Failed query $query");
+    echo $database->error;
+    //Stores it in an assossciate array
+    $output = array();
+          while($row = mysqli_fetch_assoc($result))
+          {
+              $product = array  (	"id" => $row['id'],
+                          "name" => $row['product_name'],
+                         "quantity" => $row['product_quantity'],
+                         "description" => $row['product_description'],
+                         "category" => $row['product_category'],
+                        "price" => $row['product_price']
+
+                       );
+          array_push($output,$product);
+         }
+        //JSON Encodes it.
+        $json_ouput = json_encode($output);
+        echo $json_ouput;
+
+
+
+?>
--- admin/js/searchBar.js
+++ admin/js/searchBar.js
@@ -1,18 +1,80 @@
-function searchBar(str){
-    if (str.length==0){
-          _("hide").style.display='block';
-          return;
-        }
-    else{
-          _("hide").style.display='none';
-        }
-}
 //This function sends through the letters to the AJAC function.
 function pageLoaded(str) {
   var fetchbutton = _("searchForProducts");
   if(fetchbutton){
-    fetchbutton.addEventListener("focus", searchBar(str));
+    fetchbutton.addEventListener("focus", validateNumber(str));
   }
+}
+//Checks to see if the input is number or not.
+function validateNumber(str){
+//Checks to see if it a number and the users input is greater then 0
+if (str.length > 0 && isNumber(str)){
+  console.log("Is a number and greater then 0   " + str)
+      _("searchStockTarget").style.display='block';
+      _("hide").style.display='none';
+      _("searchBarValidate").innerHTML = '';
+      searchBar(str);
+    }
+//Checks to see if the unders input is equeals to 0. This is needed otherwise the 'Must be a number would print' when it was emoty
+else if(str.length == 0){
+  console.log("Is not a number and less then 0   " + str)
+      _("hide").style.display='block';
+      _("searchStockTarget").style.display='none';
+      _("searchBarValidate").innerHTML = '';
+    }
+//If it is not a number is prints this message
+else{
+  console.log("Is not a number and less then 0   " + str)
+      _("hide").style.display='block';
+      _("searchStockTarget").style.display='none';
+      var error = _("searchBarValidate");
+      error.style.color ='red';
+      error.innerHTML = 'Must be a number';
+      return false;
+    }
+}
+//Really cool function that tests that it is a number.
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+//Runs a search on the users input.
+function searchBar(str){
+    var xhr = new XMLHttpRequest();
+    var target = document.getElementById("searchStockTarget");
+    changeListener = function () {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+       searchResults(xhr.responseText, target, str);
+    }
+  };
+  xhr.open("GET", "SQL/searchDatabaseSQL.php?str="+str, true);
+  xhr.onreadystatechange = changeListener;
+  xhr.send();
+}
+//Outputs the items the way I want it.
+function searchResults(jsonObj, target, str){
+  target.innerHTML = "";
+  var json_output = JSON.parse(jsonObj);
+  //Checks to see if anything has come back from the search. If nothing has. Prints out message.
+  if(isEmpty(json_output)){
+    target.innerHTML = "<div class='noResults'><p>You have no stock with less then " + str + " remaining!<p></div>";
+  }
+  else{
+  //Starts the loop
+  for( var i=0; i < json_output.length; i++) {
+
+    var output 	= 	"<div id='item"					+ json_output[i].id 			+"' class='item'>"	+
+            '<h2> Product Name: ' 			+ json_output[i].name			+ '</h2>'		+
+            "<p><img src='../CMS/Images/" 	+ json_output[i].id 			+ ".jpg'></p>" +
+            "<p class='bold'> Product Quantity: " 		+ json_output[i].quantity 		+ '</p>' +
+            '<p> Product Description: ' 	+ json_output[i].description  	+ '</p>' 		+
+            '<p> Product Category: ' 		+ json_output[i].category 		+ '</p>' 		+
+            '<p> Product Price: £' 			+ json_output[i].price 			+ '</p>'		+
+            "</div>";
+
+            //This outputs the array
+            target.innerHTML += output;
+  }
+}
 }
 function setListeners(){
   lowStock();
